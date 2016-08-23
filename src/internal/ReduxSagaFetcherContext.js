aa04cb345a4253a1e8fefd7f6f7b6e47979be20e
Nikita Kosolapov <ntkoso@gmail.com>
2016-08-23 03:03:35 +0300

    Fix lifecycle method
    Forgot to change  to .

--- src/internal/ReduxSagaFetcherContext.js
+++ src/internal/ReduxSagaFetcherContext.js
@@ -10,7 +10,7 @@ class ReduxSagaFetcherContext extends React.Component {
       return;
     }
 
-    this.triggerDispatch(this.props);
+    this.triggerDispatch(nextProps);
   }
 
   triggerDispatch({ dispatch, routerProps, locals }) {
